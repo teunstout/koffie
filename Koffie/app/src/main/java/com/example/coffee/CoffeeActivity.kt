b54f95e4a5671289539f3f1d522fb33bdd97987d
Teun Stout <60847859+teunstout@users.noreply.github.com>
2020-05-31 14:19:29 +0200

    Sluit af door navigatie

--- Koffie/app/src/main/java/com/example/coffee/CoffeeActivity.kt
+++ Koffie/app/src/main/java/com/example/coffee/CoffeeActivity.kt
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_coffee.*
@@ -15,6 +16,28 @@ class CoffeeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coffee)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(toolbar)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        // The NavController
+        val navController = findNavController(R.id.navHostFragment)
+
+        // Connect the navHostFragment with the BottomNavigationView.
+        NavigationUI.setupWithNavController(navView, navController)
+
+        // Connect the navHostFragment with the Toolbar.
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        // Add a Destination Changed Listener. This gets called whenever the navigation controller is navigating to another fragment.
+//        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+//            when (destination.id) {
+//                R.id.homeFragment -> showBottomNavigationBar(true)
+//                R.id.rateFragment -> showBottomNavigationBar(true)
+//                R.id.ratedFragment -> showBottomNavigationBar(false)
+//            }
+//        }
     }
 }
