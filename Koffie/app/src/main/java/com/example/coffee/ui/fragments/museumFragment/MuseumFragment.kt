e9bb7035216821a4cd742e982adb30fd2306aa50
Teun Stout <60847859+teunstout@users.noreply.github.com>
2020-06-05 16:07:43 +0200

    before firebase

--- Koffie/app/src/main/java/com/example/coffee/ui/fragments/museumFragment/MuseumFragment.kt
+++ Koffie/app/src/main/java/com/example/coffee/ui/fragments/museumFragment/MuseumFragment.kt
@@ -8,10 +8,12 @@ import android.widget.RelativeLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coffee.R
 import com.example.coffee.model.rijksMuseumObjects.MuseumObject
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_museum.*
 
 class MuseumFragment : Fragment() {
@@ -19,6 +21,7 @@ class MuseumFragment : Fragment() {
     private lateinit var dashboardViewModel: MuseumViewModel // ViewModel
     private var artifacts = ArrayList<MuseumObject>()  // Array of MuseumObjects
     private var museumAdapter = MuseumAdapter(artifacts) // Adapter instantiated with MuseumObjects
+    private var startPageArtifacts = 1
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         dashboardViewModel = ViewModelProvider(this).get(MuseumViewModel::class.java) // ViewModel of museum
@@ -49,7 +52,8 @@ class MuseumFragment : Fragment() {
         // Recyclerview with vertical layout
         rvArtifacts.layoutManager = LinearLayoutManager(this.context, RecyclerView.VERTICAL , false)
         rvArtifacts.adapter = museumAdapter // Set adapter of recyclerview
-        dashboardViewModel.getMuseumObjects(1)
+        dashboardViewModel.getMuseumObjects(startPageArtifacts)
     }
+
 }
 
